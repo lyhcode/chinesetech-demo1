bf0a969a07a52122467100cae26b86c4354c16f7
Yan-hong Lin <lyhcode@gmail.com>
2012-08-14 23:28:54 +0800

    update descriptions

--- application/views/s3listall.php
+++ application/views/s3listall.php
@@ -1,5 +1,7 @@
 <div class="container">
 	<a href="<?php echo site_url('/')?>" class="btn">back</a>
+	<hr class="soften" />
+	<pre>Location: application/view/s3listall.php, application/controller/s3service.php</pre>
 	<h2>S3: List all files</h2>
 	<ul>
 		<?php foreach($result as $key): ?>
--- application/views/s3upload.php
+++ application/views/s3upload.php
@@ -1,6 +1,9 @@
 <div class="container">
 	<a href="<?= base_url()?>" class="btn">Back</a>
+	<hr class="soften" />
+	<pre>Location: application/view/s3upload.php, application/controller/s3service.php</pre>
 	<h2>Upload files</h2>
+	<p>僅限檔案格式：*.mp4、*.ogg、*.ogv、*.mov，檔案大小限制：512MB</p>
 	<form>
 		<div class="row">
 			<div class="span6">
@@ -21,7 +24,7 @@ $(function() {
 		// General settings
 		runtimes : 'gears,flash,silverlight,browserplus,html5',
 		url : "<?= site_url('s3service/receive') ?>",
-		max_file_size : '100mb',
+		max_file_size : '512mb',
 		chunk_size : '1mb',
 		unique_names : false,
 
